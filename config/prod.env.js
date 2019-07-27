e0d21e013363d3b8aa29aa19dc9a6c746d9a227f
18166454052 <2198178942@qq.com>
2019-07-27 22:08:14 +0800

    --

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://127.0.0.1:7001/"',
-  publicPath: 'http://127.0.0.1:80/'
+  BASE_API: '"http://39.108.15.156:7001/"',
+  publicPath: 'http://39.108.15.156:80/'
 }

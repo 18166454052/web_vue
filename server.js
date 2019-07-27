6da5126598e79eebf607ca8235d173e6e95fc9e7
18166454052 <2198178942@qq.com>
2019-07-27 16:34:33 +0800

    移动适配

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(favicon(resolve('./favicon.ico')))
 app.use('/dist', express.static(resolve('./dist')))
 
-app.use('/', function (req, res) {
+app.use('*', function (req, res) {
   const html = fs.readFileSync(resolve('./dist/' + 'index.html'), 'utf-8')
   res.send(html)
 })

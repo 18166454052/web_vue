3e03a2c412bb42ef4ab3ec5d7375f64f3b53b584
18166454052 <2198178942@qq.com>
2019-07-28 16:11:12 +0800

    postcss-px2rem bug 修复

--- .postcssrc.js
+++ .postcssrc.js
@@ -10,7 +10,7 @@ module.exports = {
       rootValue: 75,
       unitPrecision: 5, // 最小精度，小数点位数
       propList: ['*'], // !不匹配属性（这里是字体相关属性不转换）
-      selectorBlackList: [],// 过滤
+      selectorBlackList: ['.category-all .title'],// 过滤
       minPixelValue:2 // 替换的最小像素值
     }
   }
